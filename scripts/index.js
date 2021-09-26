12c9a124aa6f7291519689b3a19cdff2846dd61d
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-26 23:55:19 +0200

    gallery

---
+++ scripts/index.js
@@ -0,0 +1,65 @@
+function loadImage1() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image01.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage2() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image02.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage3() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image03.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage4() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image04.html" ></object>';
+    modal.style.display = "block";
+  }
+  
+  function loadImage5() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image05.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage6() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image06.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage7() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image07.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage8() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image08.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage9() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image09.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage10() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="../src/image10.html" ></object>';
+    modal.style.display = "block";
+  }
+
+
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
